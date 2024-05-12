78e7b815e0d2f0789a9cd9d82106c01c70cc248d
おかーかじぇーぴー <kaitosatou0025@gmail.com>
2024-05-12 00:46:56 +0000

    a

--- new-package/src/okaka.ts
+++ new-package/src/okaka.ts
@@ -1,5 +1,11 @@
 export class Greeter {
-    greet(to: string): void {
-      console.log(`Hello ${to}`)
-    }
+  greet(to: string): void {
+    console.log(`Hello ${to}`)
   }
+  }
+
+export class Logeer {
+  log(to: string): void {
+    console.log(`weblog ${to}`)
+  }
+}
